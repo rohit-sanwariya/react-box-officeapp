39b8dcc2a57d6aec89f54625432af8f31d40661f
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-02-14 15:23:17 +0530

    Component for Home and Start pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import Nav from "./Components/Nav";
+import Home from "./Pages/Home";
+import Starred from "./Pages/Starred";
 function App() {
   return (
+    <div>
+      <Nav/>
     <Switch>
       <Route exact={true} path="/">
-        <h1>Home</h1>
+        <Home/>
       </Route>
 
       <Route  exact={true} path="/starred">
-        <h1>Starred</h1>
+        <Starred/>
       </Route>
       <Route   >
         <h1 style={{color:'#e32755'}}>404! Page Not found</h1>
       </Route>
     </Switch>
+    </div>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Links = [
+    {to:'/',text:'Home'},
+    {to:'/starred',text:'Starred'},
+]
+const Nav = () => {
+    return (
+        <div>
+            <ul>
+                {Links.map((link,i)=> <li key={i}>
+                   <Link to={link.to}>{link.text}</Link>
+                </li>)}
+               
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
