03cf0fdeabee2525cd4f1d43b8e28e181e7e00fe
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-02-14 17:42:10 +0530

    api connected

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ManageLayout from '../Components/ManageLayout'
 
 const Home = () => {
+    const [input, setInput] = useState('');
+    const onInputChange = (event) => {
+        setInput(event.target.value);
+        console.log(input);
+    }
+    const onSearch = async (event) => {
+         // https://api.tvmaze.com/search/shows?q=girls
+        const data = await (await fetch(` http://api.tvmaze.com/search/shows?q=${input}`)).json();
+        console.log(data);
+    }
+    const onKeyDown = (event) =>{
+        if(event.keyCode===13){
+            onSearch()
+        }
+    }
     return (
         <ManageLayout>
-           <h1>Home</h1> 
-        </ManageLayout>
+        <input onChange={onInputChange} value={input} type="text" onKeyDown={onKeyDown}/>
+       <button type="button" onClick={onSearch}>Search</button> </ManageLayout>
     )
 }
 
