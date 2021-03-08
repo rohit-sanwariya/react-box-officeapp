bda2b3c15c5f8eabf1cc6d6716f13205da9d9274
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-03-08 14:50:17 +0530

    UseState replace by useReducer

--- src/Pages/ShowPage.js
+++ src/Pages/ShowPage.js
@@ -1,40 +1,66 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useReducer} from 'react'
 import {useParams} from 'react-router-dom';
-import apiGet from '../Misc/Config';
+
+const initalState={result:null,isloading:true,error:null }
+const reducer=(prevState,action)=>{
+switch (action.type) {
+    case "FETCH_SUCCESS":{
+        return {...prevState,isloading:false,error:null,result:action.result}
+    }
+    case "FETCH_FAILED":{
+        return{...prevState,isloading:false,error:action.error}
+    }    
+        
+
+    default:
+        return prevState;
+}
+}
 //https://api.tvmaze.com/shows/1?embed[]=seasons&embed[]=cast
 const ShowPage = () => {
-    const [result, setResult] = useState(null);
     const {id} = useParams();
-    const [isloading, setIsloading] = useState(true);
-    const [error, setError] = useState(null);
-    useEffect( () =>{
-        let isMounted = true;
-     apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`)
-     .then((data)=>{
-    
-        if(isMounted)
-       { setResult(data)
-    setIsloading(false)}
-        
+    // const [result, setResult] = useState(null); 
+    // const [isloading, setIsloading] = useState(true);
+    // const [error, setError] = useState(null);
+    const fun = async() =>{
+        const data = await fetch(`https://api.tvmaze.com/shows/${id}?embed[]=seasons&embed[]=cast`);
+        const jdata = await data.json();
+        return await jdata
+      
+       
     }
-    
-     ).catch((error)=>{
-         setError(error.message);
-        setIsloading(false)});
+    const [state,dispatcher]=useReducer(reducer,initalState);
+
+    useEffect( () =>{
+   let isMounted = true;
+
+   if(isMounted){
+      fun().then((data)=>{
+        dispatcher({type:"FETCH_SUCCESS",result:data})
+        
+        }).catch((err)=>{
+            dispatcher({type:"FETCH_FAILED",error:err.message})
+           })
+     
+
+}
 
         return ()=>{
             isMounted = false;
         }
      
-    },[id]);
+    },[ id]);
 
 
-    if(isloading){
+    if(state.isloading){
         return <div>Data is Loading .....</div>
+    }
+    else if(state.error){
+        return <div>error</div>
     }
     return (
         <div>
-        
+        {state.result.url}
         </div>
     )
 }
