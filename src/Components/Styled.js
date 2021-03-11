873601c346b5ecea850f3eb780fe16879e6267e3
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-03-11 12:25:30 +0530

    FadeIn for animation

--- src/Components/Styled.js
+++ src/Components/Styled.js
@@ -1,7 +1,8 @@
+import FadeIn from "react-fade-in";
 import styled from "styled-components";
 
 
-export const FlexGrid = styled.div`
+export const FlexGrid = styled(FadeIn)`
 display: flex;
 justify-content:center;
 flex-wrap:wrap;
