4617dc17e837d5c14bb220f5a500dc83154f9aa1
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-03-12 00:59:22 +0530

    Reportwebwital file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter} from 'react-router-dom'
 import App from './App';
- 
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <HashRouter>
@@ -12,5 +12,5 @@ ReactDOM.render(
   </HashRouter>,
   document.getElementById('root')
 );
-
+reportWebVitals();
  
