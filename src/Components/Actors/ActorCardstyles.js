4672c14d9ffcf6e26f7b80ca6667d95bd2fe8e61
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-03-12 12:38:47 +0530

    Updates

--- src/Components/Actors/ActorCardstyles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { SearchCard } from "../Styled";
-export const StyledActorCard = styled(SearchCard)`
-.deathday{
-    margin:0;
-    margin-top: 15px;
-    font-weight: bold;
-}
-`
